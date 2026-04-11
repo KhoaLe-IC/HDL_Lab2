1dd03d22d660cc90ffe2a96089d2d4efb520862d
24520301 <24520301@gm.uit.edu.vn>
2026-04-11 15:33:42 +0700

    Update Topmodule.v

--- verilog bài 2/Topmodule.v
+++ verilog bài 2/Topmodule.v
@@ -1,5 +1,4 @@
-//code xử lý 1 hàng 2048 pixel thay vì 1 cột 1365 pixel
-//kiểu này đơn giản hơn do file input xử lý từng pixel theo từng hàng
+
 module Topmodule(clk,valid_in, valid_out, rgb_in, brightness, gray_out);
 	input clk;
 	input valid_in;
@@ -7,22 +6,14 @@ module Topmodule(clk,valid_in, valid_out, rgb_in, brightness, gray_out);
 	output valid_out;
 	input signed [8:0] brightness;
 	output [16383:0] gray_out;
+		
 	
-	
-	//thanh ghi valid này t lấy ra ngoài do tại mình nạp 2048 pixel cùng 1 lúc thì t nghĩ
-	//1 tín hiệu valid thôi có thể thay thế được cho 2048 cái tín hiệu trong module RGBtoGray
 	reg [3:0] Valid;
 	always @(posedge clk) begin
-		// thêm res ở đây không thì những pixel đầu cho ra xx
-		
-			Valid <= {Valid[2:0],valid_in};
-		
+		Valid <= {Valid[2:0],valid_in};
 	end
 	assign valid_out = Valid[3];
-	
-	
-	//valid[3] thì ra xx còn valid[2] thì ra đúng
-	//vào cái xung clock thứ 4 thì do valid[3] chưa được nạp giá trị nên còn là xx,
+		
 	genvar i;
 	generate 
 		for (i = 0; i < 2048; i = i+1) begin : rgb2grey_gen
